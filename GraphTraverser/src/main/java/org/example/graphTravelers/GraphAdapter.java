b2414413893a01cd742f42707ba322811172148f
arezakhanari <a.reza.khanri@gmail.com>
2025-01-12 20:05:21 +0330

    Change library and use JGraphT instead of JUNG

--- GraphTraverser/src/main/java/org/example/graphTravelers/GraphAdapter.java
+++ GraphTraverser/src/main/java/org/example/graphTravelers/GraphAdapter.java
@@ -1,14 +1,16 @@
 package org.example.graphTravelers;
 
-import edu.uci.ics.jung.graph.SparseMultigraph;
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleGraph;
 
 import java.util.Collection;
 
 public class GraphAdapter implements Graph<Integer, String> {
-    private final SparseMultigraph<Integer, String> adaptee;
+    private final org.jgrapht.Graph<Integer, DefaultEdge> adaptee;
 
     public GraphAdapter() {
-        this.adaptee = new SparseMultigraph<>();
+        this.adaptee = new SimpleGraph<>(DefaultEdge.class);
     }
 
     @Override
@@ -18,16 +20,15 @@ public class GraphAdapter implements Graph<Integer, String> {
 
     @Override
     public void addEdge(String edge, Integer v1, Integer v2) {
-        adaptee.addEdge(edge, v1, v2);
+       adaptee.addEdge(v1, v2);
     }
 
     @Override
     public Collection<Integer> getNeighbors(Integer vertex) {
-        return adaptee.getNeighbors(vertex);
+        return Graphs.neighborListOf(adaptee, vertex);
     }
 
-    // Expose the internal SparseMultigraph if needed
-    public SparseMultigraph<Integer, String> getAdaptee() {
+    public org.jgrapht.Graph<Integer, DefaultEdge> getAdaptee() {
         return adaptee;
     }
 }
